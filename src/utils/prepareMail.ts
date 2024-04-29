bfb51037d6bcda32a3657d53f4a7a5952946d5f6
Balamathias <balamathias05@gmail.com>
2024-04-29 16:09:19 +0100

    ADDED promises

--- src/utils/prepareMail.ts
+++ src/utils/prepareMail.ts
@@ -21,17 +21,24 @@ export const prepareMail = async ({email, subject, message, html }: Props) => {
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        tls: 'SSLv3',
         auth: {
             user: process.env.NEXT_GMAIL_USER,
             pass: process.env.NEXT_GMAIL_PASS,
         },
     })
 
-    transporter.sendMail(mailOptions, (err: any, info: any) => {
+    await new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, (err: any, info: any) => {
         
-        if (err) {
-          return {message: 'Connection refused', status: 404}
-        } 
+            if (err) {
+              return {message: 'Connection refused', status: 404}
+            } 
+            resolve(info)
+        })
     })
+
     return {message: 'Success', status: 201}
 }
